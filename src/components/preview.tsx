d9f5aa1d4b46513a508b14c114f794dbce65985a
Logan Barsell <loganjbars@gmail.com>
2021-11-27 21:07:19 -0800

    added markdown editor

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react';
 
 interface PreviewProps {
   code: string;
+  err: string;
 }
 
 const html = `
@@ -13,13 +14,20 @@ const html = `
   <body>
     <div id="root"></div>
     <script>
+      const handleError = (err) => {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+        console.error(err);
+      }
+      window.addEventListener('error', (event) => {
+        event.preventDefault();
+        handleError(event.error);
+      });
       window.addEventListener('message', (event) => {
         try {
           eval(event.data);
         } catch (err) {
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-          console.error(err);
+          handleError(err);
         }
       }, false);
     </script>
@@ -28,7 +36,7 @@ const html = `
 </html>
 `
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
@@ -38,11 +46,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
     }, 50);
   }, [code]);
 
+  
+
   return (
     <div className="preview-wrapper">
       <iframe 
         title="preview" ref={iframe} srcDoc={html} sandbox="allow-scripts" 
       />
+      {err && <div className="preview-error">{err}</div>}
     </div>
   );
 };
